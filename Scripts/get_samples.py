d38752eb8c571e09241eb6dea37fe65f1ec58f51
Martijn Gobes <martijn.gobes@wur.nl>
2021-11-26 12:14:59 +0100

    finished get samples

--- Scripts/get_samples.py
+++ Scripts/get_samples.py
@@ -2,6 +2,8 @@ from collections import defaultdict
 from typing import NamedTuple, Tuple, List, Dict
 from sys import argv
 from os.path import exists, join, split
+from os import remove, rename
+import random
 import subprocess
 
 #Leaf51, Axenic
@@ -18,6 +20,29 @@ Entry = NamedTuple(
         ("sample_title", str)    
     ]
 )
+
+def parse_fasta(seq_filename: str, strands: dict) -> Dict[str, Tuple[str, int]]:
+    """
+    Take a fasta file and write the data into the dictionary. Provides name, sequence and length
+
+    :param seq_filename: Name of the fasta file
+    :param strands:  dictionary to write the information into for appending
+
+    :returns Dictionary containing {name: (sequence, length), ...}
+    """
+    with open(seq_filename, "r", encoding="utf-8") as file:
+        entries = [entry.split("\n") for entry in file.read().split(">")[1:]]
+
+        for entry in entries:
+            label = entry[0].split()[0]  # Obtain just the name
+            if "|" in label:  # Some sequences can have a pipe, so we want only the last entry
+                label = label.split("|")[-1]
+            data = "".join(entry[1:])
+
+            strands[label] = (data, len(data))
+
+    return strands
+
 
 def parse_arguments() -> Tuple[str, str]:
     """
@@ -92,29 +117,50 @@ def download_samples(entries: Dict[str, List[Entry]], samples_to_get: List[str],
     return filenames        
         
 def trim_files(filenames: Dict[str, List[str]], trim_amount: float, limit=3):
+    files = defaultdict(list)
     for sample, files in filenames.items():
         for index, file in enumerate(files):
-            if index > limit:
+            if index >= limit:
                 continue
             
             filename = split(file)[1][:-3]
-            target_location = join("..", "Intermediate", "trimmed_" + filename)
-            # Unzip file to temp dir
-            if not exists(target_location) and exists(file):
-                print(f"Would have uncompressed {filename} at {file} to {target_location}")
-                #result = subprocess.check_call(f"gzip -dkc {file_location} > {target_location}", shell=True)
-                result = 0
-                if result != 0:
-                    raise Exception(f"gzip failed with error code {result}")
+            target_location = join("..", "Intermediate", "to_trim_" + filename)
+            trimmed_location = join("..", "Intermediate", "trimmed_" + filename)
+
+            if not exists(trimmed_location):
+
+                # Unzip file to temp dir
+                if not exists(target_location) and exists(file):
+                    print(f"Would have uncompressed {filename} at {file} to {target_location}")
+                    #result = subprocess.check_call(f"gzip -dkc {file_location} > {target_location}", shell=True)
+                    result = 0
+                    if result != 0:
+                        raise Exception(f"gzip failed with error code {result}")
+                else:
+                    raise Exception(f"Could not find file: {file}")
+
+
+                # Remove percentage of entries
+                # write file to proper location with _trimmed suffix
+                fastas = parse_fasta(target_location, {})
+                with open(trimmed_location, "w") as trimmed_file:
+                    samples = random.sample(fastas.items(), round(trim_amount*len(fastas)))
+                    for title, seq in samples:
+                        trimmed_file.write(f">{title}\n{seq}\n")
+
+                remove(target_location)
             else:
-                raise Exception(f"Could not find file: {file}")
+                print(f"Found trimmed file at {trimmed_location}")
 
-
-            # Remove percentage of entries
-            # write file to proper location with _trimmed suffix
+            files[sample].append(trimmed_location)
 
 def rename_files(filenames: Dict[str, List[str]]):
-    pass
+    for sample, files in filenames.items():
+        for index, file in enumerate(files):
+            new_name = join(split(file)[0], f"{sample}_{index}.fastq")
+            print(f"renaming {file} to {new_name}")
+            #rename(file, new_name)
+    
 
 def main():
     sample_filename, samples_to_get, location, remove_percentage = parse_arguments()
